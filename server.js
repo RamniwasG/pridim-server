70ee1f4983fb723cd64055f93cd7c2a3d830ccef
Ramniwas Gupta <ramniwas.nitrr15@gmail.com>
2024-10-27 16:54:16 +0530

    default route added

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ mongoose.connect(process.env.MONGODB_CLOUD_URL, {
   console.error('Error connecting to MongoDB:', error);
 });
 ;
+app.get('/', (req, res) => console.log("App is running."));
 app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter);
 app.use('/api/dashboard', dashboardRouter);
